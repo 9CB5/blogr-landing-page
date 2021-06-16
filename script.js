46d4df0c87575e183c65a32b3e460fc1805c1300
Christian Barcelona <christianbarcelona95@gmail.com>
2021-06-16 01:39:26 +0100

    various bug fixes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function toggleDropdown(dropdownID) {
+var activeDropDownClass = '';
+
+function toggleDropdown(dropdownID, dropdownClass) {
     document.getElementById(dropdownID).classList.toggle("show");
+    activeDropDownClass = dropdownClass;
 }
 
 function navToggle() {
@@ -16,10 +19,11 @@ navToggle();
   
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.link')) {
+    console.log(activeDropDownClass);
+    console.log(event.target.className);
+    if (event.target.className != activeDropDownClass) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
+      for (var i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
